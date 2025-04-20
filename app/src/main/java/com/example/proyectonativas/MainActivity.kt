4e7170447cc6ecdf0741ef7bedf4af119f36ab30
Nicodu2005 <nicolaspoveda000@gmail.com>
2025-04-20 17:43:44 -0500

    adelanto del proyecto

--- app/src/main/java/com/example/proyectonativas/MainActivity.kt
+++ app/src/main/java/com/example/proyectonativas/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.proyectonativas
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.proyectonativas.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navcontroller: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var drawerLayout: androidx.drawerlayout.widget.DrawerLayout
+    private lateinit var drawerToogle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+/*configuracion de la barra de herramientas*/
+        toolbar = findViewById(R.id.header)
+        setSupportActionBar(toolbar)
+/* navegacion de fragment*/
+        val navHostFragment= supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment2)
+
+        navcontroller = navHostFragment?.findNavController()!!
+        drawerLayout= findViewById(R.id.main)
+
+        val navView2 : NavigationView = findViewById(R.id.nav_view2)
+
+        drawerToogle = ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
+        drawerLayout.addDrawerListener(drawerToogle)
+        drawerToogle.syncState()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.favoritos,
+                R.id.productos
+            ),
+            drawerLayout
+        )
+
+        setupActionBarWithNavController(navcontroller,appBarConfiguration)
+        navView2.setupWithNavController(navcontroller)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navcontroller.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
---
+++ app/src/main/java/com/example/proyectonativas/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.example.proyectonativas
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Activity splash", "esta en estado on start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Activity splash", "esta en estado onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Activity splash", "esta en estado onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Activity splash", "esta en estado onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Activity splash", "esta en estado onDestroy")
+
+    }
+}
